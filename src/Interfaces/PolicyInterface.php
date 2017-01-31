65fddaa4b28f032ca240954c93b21e6bcd6bd610
Abdul.Mohsen B. A. A <admin@arabcoders.org>
2017-01-31 06:21:53 +0300

    Added Policy Class to Manage displaying/logging/exiting of the program for specific errors/exceptions.

---
+++ src/Interfaces/PolicyInterface.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * This file is part of ( framework ) project.
+ *
+ * (c) 2017 ArabCoders Ltd.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace arabcoders\errors\Interfaces;
+
+/**
+ * Interface PolicyInterface
+ *
+ * @package arabcoders\errors\Interfaces
+ */
+interface PolicyInterface
+{
+    /**
+     * PolicyInterface constructor.
+     *
+     * @param int        $type       {@see ErrorInterface::TYPE_ERROR} or {@see ErrorInterface::TYPE_EXCEPTION}
+     * @param string|int $parameter  Error Number or Exception FQN.
+     * @param bool       $logging    whether to enable logging or not.
+     * @param bool       $displaying whether to enable displaying if the error or not.
+     * @param bool       $exiting    whether to shutdown after encountering this error.
+     * @param \Closure   $closure    Closure to call upon encountering this error.
+     */
+    public function __construct( int $type, $parameter, bool $logging, bool $displaying, bool $exiting, \Closure $closure = null );
+
+    /**
+     * is of type.
+     *
+     * @param int $type {@see ErrorInterface::TYPE_ERROR} or {@see ErrorInterface::TYPE_EXCEPTION}
+     *
+     * @return int
+     */
+    public function isOfType( int $type ) : int;
+
+    /**
+     * Get Error Type.
+     *
+     * @return int
+     */
+    public function getType() : int;
+
+    /**
+     * Get Parameter as in Error Number or Exception Name.
+     *
+     * @return int|string
+     */
+    public function getParameter();
+
+
+    /**
+     * Allow The Logging of this error.
+     *
+     * @return bool
+     */
+    public function allowLogging() : bool;
+
+    /**
+     * Allow The Displaying of this error.
+     *
+     * @return bool
+     */
+    public function allowDisplaying() : bool;
+
+    /**
+     * Whether to shutdown the app after encountering this error.
+     *
+     * @return bool
+     */
+    public function allowExiting() : bool;
+
+    /**
+     * Does This Policy have closure?
+     *
+     * @return bool
+     */
+    public function hasClosure() : bool;
+
+    /**
+     * Get Closure.
+     *
+     * @return \Closure
+     */
+    public function getClosure() : \Closure;
+}
---
+++ src/Policy.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * This file is part of ( framework ) project.
+ *
+ * (c) 2017 ArabCoders Ltd.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace arabcoders\errors;
+
+use arabcoders\errors\Interfaces\PolicyInterface;
+
+/**
+ * Class Policy
+ *
+ * @package arabcoders\errors
+ */
+class Policy implements PolicyInterface
+{
+    /**
+     * @var int
+     */
+    private $type;
+
+    /**
+     * @var int|string
+     */
+    private $parameter;
+
+    /**
+     * @var bool
+     */
+    private $logging;
+
+    /**
+     * @var bool
+     */
+    private $displaying;
+
+    /**
+     * @var bool
+     */
+    private $exiting;
+
+    /**
+     * @var \Closure
+     */
+    private $closure;
+
+    public function __construct( int $type, $parameter, bool $logging, bool $displaying, bool $exiting, \Closure $closure = null )
+    {
+        if ( !is_string( $parameter ) && !is_int( $parameter ) )
+        {
+            throw new \InvalidArgumentException( 'Parameter Type is not string or int.' );
+        }
+
+        $this->type       = $type;
+        $this->parameter  = $parameter;
+        $this->logging    = $logging;
+        $this->displaying = $displaying;
+        $this->exiting    = $exiting;
+        $this->closure    = $closure;
+    }
+
+    public function isOfType( int $type ) : int
+    {
+        return $this->getType() === $type;
+    }
+
+    public function getType() : int
+    {
+        return $this->type;
+    }
+
+    public function getParameter()
+    {
+        return $this->parameter;
+    }
+
+    public function allowLogging() : bool
+    {
+        return $this->logging;
+    }
+
+    public function allowDisplaying() : bool
+    {
+        return $this->displaying;
+    }
+
+    public function allowExiting() : bool
+    {
+        return $this->exiting;
+    }
+
+    public function hasClosure() : bool
+    {
+        return ( $this->closure instanceof \Closure );
+    }
+
+    public function getClosure() : \Closure
+    {
+        return $this->closure;
+    }
+}
