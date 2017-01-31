5fbcb64c8913d8c749016072e001dc59169a16a0
Abdul.Mohsen B. A. A <admin@arabcoders.org>
2017-01-31 06:20:19 +0300

    mapped php error levels to syslog Severity level

--- src/Logging/Syslog.php
+++ src/Logging/Syslog.php
@@ -9,8 +9,10 @@
  */
 namespace arabcoders\errors\Logging;
 
-use arabcoders\errors\Interfaces\MapInterface;
-use arabcoders\errors\Logging\Interfaces\LoggingInterface;
+use arabcoders\errors\
+{
+    Interfaces\ErrorInterface, Interfaces\MapInterface, Logging\Interfaces\LoggingInterface
+};
 
 /**
  * Class Syslog
@@ -24,13 +26,48 @@ class Syslog implements LoggingInterface
      */
     private $map;
 
+    /**
+     * @var bool
+     */
+    private $syslog;
+
+    /**
+     * @var array Map PHP Errors to syslog.
+     */
+    private $logMap = [
+        E_ERROR             => LOG_ERR,
+        E_WARNING           => LOG_WARNING,
+        E_PARSE             => LOG_CRIT,
+        E_NOTICE            => LOG_NOTICE,
+        E_CORE_ERROR        => LOG_ALERT,
+        E_CORE_WARNING      => LOG_WARNING,
+        E_COMPILE_ERROR     => LOG_ALERT,
+        E_COMPILE_WARNING   => LOG_WARNING,
+        E_USER_ERROR        => LOG_ERR,
+        E_USER_WARNING      => LOG_WARNING,
+        E_USER_NOTICE       => LOG_NOTICE,
+        E_STRICT            => LOG_DEBUG,
+        E_RECOVERABLE_ERROR => LOG_ERR,
+        E_DEPRECATED        => LOG_DEBUG,
+    ];
+
     public function __construct( $appName = 'PHPErrors' )
     {
-        openlog( $appName, \LOG_PID, \LOG_USER );
+        if ( function_exists( 'openlog' ) )
+        {
+            openlog( $appName, \LOG_PID, \LOG_USER );
+        }
+
+        $this->syslog = function_exists( 'syslog' );
     }
 
     public function process() : LoggingInterface
     {
+        if ( !function_exists( 'syslog' ) )
+        {
+            throw new \RuntimeException( 'syslog(): function does not exists' );
+        }
+
         $message = $this->getMap()->getMessage();
 
         if ( $this->getMap()->getStructured() )
@@ -43,7 +80,16 @@ class Syslog implements LoggingInterface
             $message .= PHP_EOL . print_r( $this->getMap()->getTrace(), true );
         }
 
-        syslog( \LOG_ERR, $message );
+        if ( $this->getMap()->getType() == ErrorInterface::TYPE_EXCEPTION )
+        {
+            $logType = \LOG_ERR;
+        }
+        else
+        {
+            $logType = $this->logMap[$this->getMap()->getError()->getNumber()] ?? \LOG_ERR;
+        }
+
+        syslog( $logType, $message );
 
         return $this;
     }
