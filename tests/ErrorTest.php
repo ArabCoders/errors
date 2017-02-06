88e036f0544a9f9a591efcf4778e7b1fac3d8cbf
Abdul.Mohsen B. A. A <admin@arabcoders.org>
2017-02-06 18:30:23 +0300

    added tests for more cases

--- tests/ErrorTest.php
+++ tests/ErrorTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use arabcoders\errors\Error;
+use arabcoders\errors\Interfaces\ErrorInterface;
 use arabcoders\errors\Interfaces\FormatterInterface;
 use arabcoders\errors\Interfaces\MapInterface;
 use arabcoders\errors\Interfaces\PolicyInterface;
@@ -11,7 +12,7 @@ use arabcoders\errors\Logging\Interfaces\LoggingInterface;
 use arabcoders\errors\Output\Basic;
 use arabcoders\errors\Output\Interfaces\OutputInterface;
 
-class ErrorTest extends \PHPUnit_Framework_TestCase
+class ErrorTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @var Error
@@ -119,14 +120,164 @@ class ErrorTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals( self::$i, self::$i->register() );
     }
+
+    public function testAddSpecialCase()
+    {
+        $this->assertEquals( self::$i, self::$i->addSpecialCase( PDOException::class, 'PDO', new SpecialCase() ) );
+    }
+
+    public function testSpecialCaseBeingCalled()
+    {
+        $specialCase = new SpecialCase();
+
+        self::$i->addSpecialCase( LogicException::class, 'PDO', $specialCase );
+        self::$i->setOutput( new nullOutput() );
+        self::$i->addPolicy(
+            LogicException::class,
+            new \arabcoders\errors\Policy( ErrorInterface::TYPE_EXCEPTION, LogicException::class, false, false, false )
+        );
+
+        self::$i->handleException( new LogicException( 'foo' ) );
+        $this->assertEquals( true, $specialCase->called );
+        $this->assertEquals( self::$i->getMap(), $specialCase->getMap() );
+    }
+
+    public function testDeleteSpecialCaseNoSubSection()
+    {
+        $this->expectException( InvalidArgumentException::class );
+
+        self::$i->deleteSpecialCase( LogicException::class, 'foo' );
+    }
+
+    public function testDeleteSpecialCaseNoCase()
+    {
+        $this->expectException( InvalidArgumentException::class );
+
+        self::$i->addSpecialCase( LogicException::class, 'foo', new SpecialCase() );
+
+        self::$i->deleteSpecialCase( LogicException::class, 'bar' );
+    }
+
+    public function testDeleteSpecialCase()
+    {
+        self::$i->addSpecialCase( LogicException::class, 'foo', new SpecialCase() );
+
+        $this->assertEquals( self::$i, self::$i->deleteSpecialCase( LogicException::class, 'foo' ) );
+    }
+
+    public function testAddLoggerSuccess()
+    {
+        $this->assertEquals( self::$i, self::$i->addLogger( 'logger', new Logger() ) );
+    }
+
+    public function testAddLoggerFail()
+    {
+        $this->expectException( TypeError::class );
+
+        $this->assertEquals( self::$i, self::$i->addLogger( 'logger', self::$i ) );
+    }
+
+    public function testDeleteLoggerSuccess()
+    {
+        $this->assertEquals( self::$i, self::$i->addLogger( 'logger', new Logger() ) );
+    }
+
+    public function testDeleteLoggerFail()
+    {
+        $this->expectException( \InvalidArgumentException::class );
+
+        self::$i->deleteLogger( 'bar' );
+    }
+
+    public function testLoggerBeingCalled()
+    {
+        $logger = new Logger();
+
+        self::$i->addLogger( Logger::class, $logger );
+        self::$i->setOutput( new nullOutput() );
+        self::$i->addPolicy(
+            LogicException::class,
+            new \arabcoders\errors\Policy( ErrorInterface::TYPE_EXCEPTION, LogicException::class, true, false, false )
+        );
+
+        self::$i->handleException( new LogicException( 'foo' ) );
+        $this->assertEquals( true, $logger->called );
+        $this->assertEquals( self::$i->getMap(), $logger->getMap() );
+    }
+
+
+    public function testAddPolicySuccess()
+    {
+        $this->assertEquals( self::$i,
+                             self::$i->addPolicy(
+                                 LogicException::class,
+                                 new Policy( ErrorInterface::TYPE_EXCEPTION, LogicException::class, false, false, false ) )
+        );
+
+    }
+
+    public function testAddPolicyFail()
+    {
+        $this->expectException( TypeError::class );
+        $this->assertEquals( self::$i, self::$i->addPolicy( LogicException::class, self::$i ) );
+    }
+
+    public function testDeleteDeletePolicyNoSubSection()
+    {
+        $this->expectException( InvalidArgumentException::class );
+        $this->expectExceptionMessage( sprintf( '(%s) has no registered Policies.', LogicException::class ) );
+
+        self::$i->deletePolicy( LogicException::class, 'foo' );
+    }
+
+    public function testDeletePolicyNoPolicy()
+    {
+        $this->expectException( InvalidArgumentException::class );
+        $this->expectExceptionMessage( sprintf( '(%s) has no registered Policy of name (%s).', LogicException::class, 'bar' ) );
+
+        self::$i->addPolicy( LogicException::class,
+                             new Policy( ErrorInterface::TYPE_EXCEPTION, LogicException::class, false, false, false )
+        );
+
+        self::$i->deletePolicy( LogicException::class, 'bar' );
+    }
+
+    public function testPolicyBeingCalled()
+    {
+        self::$i->setOutput( new nullOutput() )
+                ->addPolicy( LogicException::class, new Policy( ErrorInterface::TYPE_EXCEPTION, LogicException::class, false, false, false ) )
+                ->addPolicy( InvalidArgumentException::class, new Policy( ErrorInterface::TYPE_EXCEPTION, InvalidArgumentException::class, true, true, false ) );
+
+        try
+        {
+            throw new LogicException( 'foo' );
+        }
+        catch ( Throwable $e )
+        {
+            $this->assertEquals( self::$i, self::$i->handleException( $e ) );
+        }
+
+        try
+        {
+            $this->assertNotEquals( self::$i, self::$i->handleException( new InvalidArgumentException() ) );
+        }
+        catch ( Throwable $e )
+        {
+            $this->assertEquals( true, true );
+        }
+    }
 }
 
 class Logger implements LoggingInterface
 {
     private $map;
 
+    public $called = false;
+
     public function process() : LoggingInterface
     {
+        $this->called = true;
+
         return $this;
     }
 
@@ -155,9 +306,11 @@ class SpecialCase implements SpecialCaseInterface
 {
     private $map;
 
+    public $called = false;
+
     public function handle()
     {
-        // TODO: Implement handle() method.
+        return $this->called = true;
     }
 
     public function setMap( MapInterface $map ) : SpecialCaseInterface
@@ -248,5 +401,27 @@ class Policy implements PolicyInterface
     public function getClosure() : \Closure
     {
         return $this->closure;
+    }
+}
+
+class nullOutput implements OutputInterface
+{
+    private $map;
+
+    public function display()
+    {
+        return '';
+    }
+
+    public function setMap( MapInterface $map ) : OutputInterface
+    {
+        $this->map = $map;
+
+        return $this;
+    }
+
+    public function getMap() : MapInterface
+    {
+        return $this->map;
     }
 }
